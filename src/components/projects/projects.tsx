b6a4750f63ee13d670a59fc9d863dab213e07699
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-12-14 16:15:15 -0300

    toSpliced not works on Vercel

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -6,6 +6,8 @@ import { projects } from '@/libs/project';
 type Props = {};
 
 const Projects = (props: Props) => {
+  const sliceProjects = projects.slice(0, 3);
+
   return (
     <main className="screens mb-24">
       <h2 className="text-4xl font-bold mb-4">
@@ -14,7 +16,10 @@ const Projects = (props: Props) => {
       </h2>
 
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 justify-center">
-        {projects.toSpliced(3).map((project) => (
+        {/* {projects.toSpliced(3).map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))} */}
+        {sliceProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
